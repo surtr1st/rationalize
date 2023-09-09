dfe692a5758917a8e43bb2174282433f95cf4cfd
Ishi Tran <berugen1st@gmail.com>
2023-09-09 13:58:31 +0700

    add vital module in lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,28 @@
-pub mod analysis;
-pub mod comparison;
-pub mod isolation;
+pub mod rationalize {
+    use std::collections::hash_map::DefaultHasher;
+    use std::hash::{Hash, Hasher};
+
+    #[tauri::command]
+    pub fn read_hash(data: &Vec<String>) {}
+
+    #[tauri::command]
+    pub fn compare(data: &Vec<String>) {}
+
+    #[tauri::command]
+    pub fn create_folder() {}
+
+    #[tauri::command]
+    pub fn transfer_duplication() {}
+
+    #[tauri::command]
+    pub fn export_location() {}
+
+    #[tauri::command]
+    pub fn exec() {}
+
+    pub fn hash<T: Hash>(t: &T) -> u64 {
+        let mut s = DefaultHasher::new();
+        t.hash(&mut s);
+        s.finish()
+    }
+}
