07426e64dd89d936efbeec25f7237560098907c9
Ishi Tran <berugen1st@gmail.com>
2023-09-11 10:32:22 +0700

    separate lib utils to helpers

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,11 @@
-use blake3::Hasher;
+pub mod helpers;
 use fs_extra::file;
+use helpers::*;
 use rayon::prelude::*;
 use std::collections::{hash_map::HashMap, hash_set::HashSet};
 use std::env;
-use std::fs::{self, File};
-use std::io::Read;
-use std::path::{Path, PathBuf};
+use std::fs;
+use std::path::Path;
 use std::process::Command;
 
 const WINDOWS_EXPLORER: &str = "explorer";
@@ -100,44 +100,3 @@ pub fn open_location(target_dir: String) {
 }
 
 pub fn exec() {}
-
-fn hash(file_path: &str) -> Result<String, std::io::Error> {
-    let mut file = File::open(file_path)?;
-    let mut hasher = Hasher::new();
-    let mut buffer = [0u8; 4096];
-
-    loop {
-        let bytes_read = file.read(&mut buffer)?;
-        if bytes_read == 0 {
-            break;
-        }
-        hasher.update(&buffer[..bytes_read]);
-    }
-    let result = hasher.finalize();
-    Ok(format!("{result}"))
-}
-
-fn retrieve_directory_content(dir: &str) -> Vec<PathBuf> {
-    fs::read_dir(dir)
-        .expect("should read the directory specified!")
-        .map(|entry| entry.unwrap().path())
-        .filter(|path| path.is_file() || path.is_dir())
-        .collect::<Vec<_>>()
-}
-
-fn convert_to_linux_path(path: &str) -> String {
-    let path_str = path.to_string();
-    path_str.replace('\\', "/")
-}
-
-fn is_windows_path(path: &str) -> bool {
-    path.contains('\\')
-}
-
-fn convert_between_linux_and_windows(path: &PathBuf) -> String {
-    let path = path.to_str().unwrap();
-    if is_windows_path(path) {
-        return convert_to_linux_path(path);
-    }
-    path.to_string()
-}
