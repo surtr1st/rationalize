d603c6dac5176cb4b5701e50383c9631274ad0e6
Ishi Tran <berugen1st@gmail.com>
2023-09-12 07:42:33 +0700

    add boilerplate test functions

---
+++ src-tauri/tests/main.rs
@@ -0,0 +1,18 @@
+#[cfg(test)]
+mod rationalize_on_action {
+
+    #[test]
+    fn try_read_hash_from_input() {}
+
+    #[test]
+    fn try_find_duplicate_values() {}
+
+    #[test]
+    fn try_create_folder_or_directory() {}
+
+    #[test]
+    fn try_transfer_duplicates() {}
+
+    #[test]
+    fn try_open_location() {}
+}
