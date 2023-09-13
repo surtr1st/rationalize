52ffb5051a04b846832042afd7bdf6efc09e388b
Ishi Tran <berugen1st@gmail.com>
2023-09-13 12:59:39 +0700

    add client handler

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,5 @@
-use leptos::leptos_dom::ev::{SubmitEvent};
-use leptos::*;
-use serde::{Deserialize, Serialize};
-use serde_wasm_bindgen::to_value;
+//use leptos::leptos_dom::ev::{SubmitEvent};
+use leptos::{ev::Event, *};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -10,8 +8,34 @@ extern "C" {
     async fn invoke(cmd: &str, args: JsValue) -> JsValue;
 }
 
-
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
+    let (target_dir, set_target_dir) = create_signal(cx, String::new());
 
+    let handle_upload_directory = move |event: Event| {
+        if let Some(current_target) = event.current_target() {
+            println!("{:?}", current_target);
+        }
+    };
+
+    view! { cx,
+        <main class="container">
+            <input
+                type="text"
+                placeholder="Directory"
+                value=move || target_dir.get()
+            />
+            <input
+                type="file"
+                class="directory-upload"
+                on:change=handle_upload_directory
+            />
+            <label
+                for="target_dir"
+                class="directory-upload-button"
+            >
+                "Choose directory"
+            </label>
+        </main>
+    }
 }
